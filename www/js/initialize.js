590291ecf5142b2a582c157516e241428ad32005
= <=>
2018-07-11 17:18:50 +0200

    1st release

--- www/js/initialize.js
+++ www/js/initialize.js
@@ -113,22 +113,22 @@ function pagebeforecreate(){
 	                				   var msg = 'Error al descargar: ';
 									   switch (e.code) {
 									     case FileError.QUOTA_EXCEEDED_ERR:
-									       msg = 'QUOTA_EXCEEDED_ERR';
+									       msg += 'QUOTA_EXCEEDED_ERR';
 									       break;
 									     case FileError.NOT_FOUND_ERR:
-									       msg = 'NOT_FOUND_ERR';
+									       msg += 'NOT_FOUND_ERR';
 									       break;
 									     case FileError.SECURITY_ERR:
-									       msg = 'SECURITY_ERR';
+									       msg += 'SECURITY_ERR';
 									       break;
 									     case FileError.INVALID_MODIFICATION_ERR:
-									       msg = 'INVALID_MODIFICATION_ERR';
+									       msg += 'INVALID_MODIFICATION_ERR';
 									       break;
 									     case FileError.INVALID_STATE_ERR:
-									       msg = 'INVALID_STATE_ERR';
+									       msg += 'INVALID_STATE_ERR';
 									       break;
 									     default:
-									       msg = 'Unknown Error';
+									       msg += 'Unknown Error';
 									       break;
 									   };
 									   alert(e.source+" "+e.target+" "+msg);
@@ -141,22 +141,22 @@ function pagebeforecreate(){
 	                				   var msg = 'No existe  '+store + data.friendly_url;
 									   switch (e.code) {
 									     case FileError.QUOTA_EXCEEDED_ERR:
-									       msg = 'QUOTA_EXCEEDED_ERR';
+									       msg += 'QUOTA_EXCEEDED_ERR';
 									       break;
 									     case FileError.NOT_FOUND_ERR:
-									       msg = 'NOT_FOUND_ERR';
+									       msg += 'NOT_FOUND_ERR';
 									       break;
 									     case FileError.SECURITY_ERR:
-									       msg = 'SECURITY_ERR';
+									       msg += 'SECURITY_ERR';
 									       break;
 									     case FileError.INVALID_MODIFICATION_ERR:
-									       msg = 'INVALID_MODIFICATION_ERR';
+									       msg += 'INVALID_MODIFICATION_ERR';
 									       break;
 									     case FileError.INVALID_STATE_ERR:
-									       msg = 'INVALID_STATE_ERR';
+									       msg += 'INVALID_STATE_ERR';
 									       break;
 									     default:
-									       msg = 'Unknown Error';
+									       msg += 'Unknown Error';
 									       break;
 									   };
 									   alert(e.source+" "+e.target+" "+msg);
