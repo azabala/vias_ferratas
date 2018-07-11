b9c846d7b6ed30500f4e1bf5c0154fe9451bb058
= <=>
2018-07-11 15:19:30 +0200

    1st release

--- www/js/initialize.js
+++ www/js/initialize.js
@@ -109,7 +109,32 @@ function pagebeforecreate(){
 	                    				    }
 	                    		    );
 	                			},
-	                			errorHandler
+	                			function(){
+	                				   var msg = 'Error al descargar: ';
+									   switch (e.code) {
+									     case FileError.QUOTA_EXCEEDED_ERR:
+									       msg = 'QUOTA_EXCEEDED_ERR';
+									       break;
+									     case FileError.NOT_FOUND_ERR:
+									       msg = 'NOT_FOUND_ERR';
+									       break;
+									     case FileError.SECURITY_ERR:
+									       msg = 'SECURITY_ERR';
+									       break;
+									     case FileError.INVALID_MODIFICATION_ERR:
+									       msg = 'INVALID_MODIFICATION_ERR';
+									       break;
+									     case FileError.INVALID_STATE_ERR:
+									       msg = 'INVALID_STATE_ERR';
+									       break;
+									     default:
+									       msg = 'Unknown Error';
+									       break;
+									   };
+									   alert(e.source+" "+e.target+" "+msg);
+									   $.mobile.loading( 'hide' );
+	                			
+	                			}
 	            		);
 				   });					  
 		}, errorHandler);//requestFileSystem
